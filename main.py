b982de3a7da675da9287a051ed1509f5cd38b357
Németh Dávid <122981128+nmthdavid@users.noreply.github.com>
2023-02-20 09:55:02 +0100

    Add files via upload
    In this final version I improved the way the program processed the date, now it uses JSON fomats

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import json
+
 
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
@@ -31,22 +33,52 @@ def generate_password():
         password += char
 
     password_entry.insert(0,password)
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
 def save():
 
     website_input = website_entry.get()
     mail_input = email_entry.get()
     password_input = password_entry.get()
+    new_dict = {
+        website_input: {
+            "email": mail_input,
+            "password": password_input,
+        }
+    }
 
     if website_input == "" or mail_input == "" or password_input == "" :
         messagebox.showinfo(title="Oops", message="Don't leave any fields empty")
+    else:
+        try:
+            with open("data.json","r") as file:
+                data = json.load(file)
 
-    with open("passwords.txt","a") as file:
-        datas = f"{website_input} | {mail_input} | {password_input}\n"
-        file.write(datas)
+        except FileNotFoundError:
+            with open("data.json","w") as file:
+                json.dump(new_dict, file, indent=4)
+        else:
+            data.update(new_dict)
+            with open("data.json", "w") as file:
+                json.dump(data, file, indent=4)
 
-        website_entry.delete(0,END)
-        password_entry.delete(0,END)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+def search():
+    website = website_entry.get()
+    with open("data.json") as data_file:
+        data = json.load(data_file)
+        if website in data:
+            email = data[website]
+            password = data[website]["password"]
+            mail = data[website]["email"]
+            email_entry.delete(0,END)
+            email_entry.insert(0,mail)
+            password_entry.insert(0,password)
+
 
 window = Tk()
 window.title("Password Manager")
@@ -60,9 +92,9 @@ canvas.grid(row=0,column=1)
 website_label = Label(text="Website:")
 website_label.grid(row=1,column=0)
 
-website_entry = Entry(width=38)
+website_entry = Entry(width=21)
 website_entry.focus()
-website_entry.grid(row=1,column=1,columnspan=2)
+website_entry.grid(row=1,column=1)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2,column=0)
@@ -82,5 +114,8 @@ generate_button.grid(row=3,column=2)
 
 add_button = Button(text="Add",width=36,command=save)
 add_button.grid(row=4,column=1,columnspan=2)
+
+search_button = Button(text="Search",width=13,command=search)
+search_button.grid(row=1,column=2)
 
 window.mainloop()
